0727f7a3e85f2f7fcb53d01fa5ac76e2e026da4f
Geo99pro <lolo@aluno.puc-rio.br>
2025-09-19 13:08:49 -0300

    feat: initialize the project with main module and metadata

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+import os
+import sys
+__version__ = "1.0.0"
+__maintainer__ = "Loïck G. Hodonou"
+__collaborators__ = "Victor"
+__email__ = "lhodonou349@gmail.com"
+__date__ = "2025-06-26"
+__development_status__ = "In progress"
+__credits__ = "Self contribution"
+__license__ = "MIT License"
+
+print(f"Welcome to Noisy Labels {__version__} developed and maintained by {__maintainer__}.")
