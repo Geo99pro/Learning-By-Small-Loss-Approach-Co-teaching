8c06762e21ac49d18f0dcf0636315f2a976f3047
Geo99pro <lolo@aluno.puc-rio.br>
2025-09-20 16:23:28 -0300

    feat: implement model evaluation and multilabel metrics calculation

---
+++ src/metric.py
@@ -0,0 +1,63 @@
+import torch
+from sklearn.metrics import f1_score, accuracy_score
+
+@torch.no_grad()
+def evaluate_model(data_loader,
+                model_A, 
+                model_B=None,
+                threshold=0.5,
+                device='cuda'):
+    
+    model_A.eval()
+    if model_B is not None: model_B.eval()
+    total_f1_micro, total_f1_macro = 0.0, 0.0
+    total_subset_acc, total_sample_acc = 0.0, 0.0
+    n_batches = 0
+
+    for img, label in data_loader:
+        img, label = img.to(device), label.to(device)
+
+        probs_A, _ = model_A(img)
+
+        if model_B is None:
+            probs = probs_A
+        else:
+            probs_B, _ = model_B(img)
+            probs = (probs_A + probs_B) / 2.0
+            
+        preds = (probs > threshold).float()
+        f1_micro, f1_macro, subset_acc, sample_acc = multilabel_metrics_from_preds(preds, label, threshold)
+        
+        total_f1_micro += f1_micro
+        total_f1_macro += f1_macro
+        total_subset_acc += subset_acc
+        total_sample_acc += sample_acc
+        n_batches += 1
+
+    return {
+        "f1_micro": total_f1_micro / n_batches,
+        "f1_macro": total_f1_macro / n_batches,
+        "subset_acc": total_subset_acc / n_batches,
+        "sample_acc": total_sample_acc / n_batches,
+    }
+
+def multilabel_metrics_from_preds(preds, labels):
+    """
+    Compute multilabel classification metrics from predicted and true labels.
+
+    Args:
+        preds (torch.Tensor): The predicted logits of shape (num_samples, num_classes).
+        labels (torch.Tensor): The true binary labels of shape (num_samples, num_classes).
+        threshold (float): The threshold to convert probabilities to binary predictions.
+
+    Returns:
+        dict: A dictionary containing the computed metrics (F1 score, accuracy, etc.).
+    """
+
+
+    f1_micro = f1_score(labels.detach().cpu(), preds.detach().cpu(), average='micro', zero_division=0)
+    f1_macro = f1_score(labels.detach().cpu(), preds.detach().cpu(), average='macro', zero_division=0)
+    subset_acc = accuracy_score(labels.detach().cpu(), preds.detach().cpu())
+    sample_acc = (preds.detach().cpu() == labels.detach().cpu()).float().mean().item()
+
+    return f1_micro, f1_macro, subset_acc, sample_acc
