bdaf6395e747702ff4853b06833efa7a04bf95c1
Geo99pro <lolo@aluno.puc-rio.br>
2025-09-24 10:28:03 -0300

    feat: increase default batch size from 32 to 64 for improved training efficiency

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -16,7 +16,7 @@ class StandardParams(Enum):
     IMG_SIZE = (224, 224)
     MEAN = (0.485, 0.456, 0.406)
     STD = (0.229, 0.224, 0.225)
-    BATCH_SIZE = 32
+    BATCH_SIZE = 64
 
 class Utils:
     @staticmethod
